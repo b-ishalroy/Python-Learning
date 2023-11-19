958dd66e1672256f76adbde9923be8a14e20ddd4
b-ishalroy <broy78244@gmail.com>
2023-11-20 01:53:23 +0530

    first commit in python file

---
+++ p1.py
@@ -0,0 +1,35 @@
+# print("Hello world")
+# print("Hello,\nWorld!")
+# print ("BIshal\n + \n❤️")
+# print ("Bishal\n")
+# print ("Roy")
+
+# print("Hello,",end=" ")
+
+# print("World!")
+
+
+# # if else code 
+# a=10
+# b=20
+# if a<b:
+#   print("a is not good student")
+# else :
+#   print("b is good student")
+
+
+# # function code 
+first_name="Bishal"
+second_name="Roy"
+full_name=first_name+" " +second_name
+print(full_name)
+
+
+# diffrent method
+first_name, last_name = "❤️", "Yoo"
+full_name = first_name +"  "+ last_name
+print(full_name)
+
+
+
+
