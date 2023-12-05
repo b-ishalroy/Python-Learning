b2d21181ae6e8c8aebd3fac7f01cc8d74a7dfce5
b-ishalroy <broy78244@gmail.com>
2023-12-05 23:48:59 +0530

    adding more code in loop files

--- loop.py
+++ loop.py
@@ -1,61 +1,74 @@
-# for loop 
-# range (start, end, step) 
+# # for loop 
+# # range (start, end, step) 
 
-for i in range (1,11):
-    print("Hello world = ", i)
-
-
-for i in range (1,11,2):
-    print("Hello world = ", i)
+# for i in range (1,11):
+#     print("Hello world = ", i)
 
 
-# mention sequence 
-list = [20,30,40,50]
-for i in list:
-    print(i)
+# for i in range (1,11,2):
+#     print("Hello world = ", i)
 
 
-
-    # While Loop 
-i=0
-while i<10:
-    print(i)    
-    i +=2
+# # mention sequence 
+# list = [20,30,40,50]
+# for i in list:
+#     print(i)
 
 
 
-# continue, it is use when x==y condition this will skip
-x=4
-y=0
-while x>=0:
-    x-=1
-    y+=1
-
-    if x==y:
-        continue
-    else:
-        print(x,y)
-
-
-# break = stop the code when condition comes
-x=4
-y=0
-while x>=0:
-    x-=1
-    y+=1
-
-    if x==y:
-        break
-    else:
-        print(x,y)
+#     # While Loop 
+# i=0
+# while i<10:
+#     print(i)    
+#     i +=2
 
 
 
-# print the give pattern 
-n=int(input("Enter number = "))
-for n in range(n):
-    print("*" * 5)
+# # continue, it is use when x==y condition this will skip
+# x=4
+# y=0
+# while x>=0:
+#     x-=1
+#     y+=1
 
-n=int(input("Enter Number = "))
+#     if x==y:
+#         continue
+#     else:
+#         print(x,y)
 
+
+# # break = stop the code when condition comes
+# x=4
+# y=0
+# while x>=0:
+#     x-=1
+#     y+=1
+
+#     if x==y:
+#         break
+#     else:
+#         print(x,y)
+
+
+
+# # print the give pattern 
+# # n=int(input("Enter number = "))
+# # for n in range(n):
+# #     print("*" * 5)
+
+
+# # see this code for end properties 
+# print("Hello,",end=" ")
+# print("World!")
+
+# for i in range (1,4,1):
+#     for j in range(1,6,1):
+#           print("*", end="")
+#     print()
+
+
+for i in range (1,5,1):
+     for j in range (1,5,1):
+          print(j, end=" ")
+     print()     
 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,5 +1,1 @@
-yoo=int(input("fed"))
-i=1
-while i<=yoo:
-    print(i)
-    i+=1
+    print()
