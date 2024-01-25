7d2a008a67d8ef07a1d4ea89ecb1ad447941ddb2
b-ishalroy <broy78244@gmail.com>
2024-01-25 16:26:36 +0530

    add code

--- question practice/q.py
+++ question practice/q.py
@@ -6,12 +6,21 @@ def factorial_number(n):
     else:
         for i in range(1,n+1):
             ans*=i
-
         return ans
         
 n=int(input("Enter first number="))        
 output=factorial_number(n)
 print("The factorial number is=",output)
 
+
+
 n=int(input("Enter number="))
+answer=1
+for i in range (1,n+5):              #logic behind this code = 2*3*4*5*6 =720 (when inpur of n is 2)
+    answer*=i
+print(answer)
+
+
+
+
 
